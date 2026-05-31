9eaec792a2e2b1332319ad80f47590071966c8c3
sumit <kalesumit8698@gmail.com>
2026-05-31 12:49:04 +0530

    Add SQL queries for customer and order data retrieval
    Added various SQL queries for retrieving customer and order data, including filtering by score and country.

---
+++ Day01_Basic_Select.sql
@@ -0,0 +1,63 @@
+-- ==========================================
+-- 📊 DAY 1: MY ALL SQL CODES
+-- ==========================================
+
+-- 📁 PROBLEM 1: Revising the Select Query 
+/* ==============================================================================
+   SELECT ALL COLUMNS
+=============================================================================== */
+
+-- Retrieve All Customer Data
+SELECT *
+FROM customers
+
+-- Retrieve All Order Data
+SELECT *
+FROM orders
+
+/* ==============================================================================
+   SELECT FEW COLUMNS
+=============================================================================== */
+
+-- Retrieve each customer's name, country, and score.
+SELECT 
+    first_name,
+    country, 
+    score
+FROM customers
+
+/* ==============================================================================
+   WHERE
+=============================================================================== */
+
+-- Retrieve customers with a score not equal to 0
+SELECT *
+FROM customers
+WHERE score != 0
+
+-- Retrieve customers from Germany
+SELECT *
+FROM customers
+WHERE country = 'Germany'
+
+-- Retrieve the name and country of customers from Germany
+SELECT
+    first_name,
+    country
+FROM customers
+WHERE country = 'Germany'
+
+
+
+-- 📁 PROBLEM 2: (Yahan apne dusre sawal ka naam likhein)
+-- Jo code aapne pehle se likha hai, use yahan paste karein:
+
+
+
+-- 📁 PROBLEM 3: (Yahan apne teesre sawal ka naam likhein)
+-- Jo code aapne pehle se likha hai, use yahan paste karein:
+
+
+
+-- 📁 PROBLEM 4: (Yahan apne chauthe sawal ka naam likhein)
+-- Jo code aapne pehle se likha hai, use yahan paste karein:
